6d50c106a60a80e5a269d581d498c1ff99bfca2d
Henry Whitaker <henrywhitaker3@outlook.com>
2022-07-26 17:46:00 +0100

    get it to ignore changes to disk, user, keys and network

--- terraform/pihole.tf
+++ terraform/pihole.tf
@@ -14,6 +14,15 @@ resource "proxmox_vm_qemu" "pihole" {
   cpu               = "host"
   memory            = 2048
 
+  lifecycle {
+    ignore_changes = [
+      network,
+      ciuser,
+      sshkeys,
+      disk
+    ]
+  }
+
   # Cloud Init Settings
   ipconfig0 = "ip=10.0.0.2/24,gw=10.0.0.1"
   nameserver = "1.1.1.1 8.8.8.8"
