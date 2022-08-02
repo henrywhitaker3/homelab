8c4b533d08c2cd27d996893f467f1a7400b2951d
Henry Whitaker <henrywhitaker3@outlook.com>
2022-08-02 19:12:40 +0100

    move alma to node 2 and update galera mem

--- terraform/alma.tf
+++ terraform/alma.tf
@@ -2,7 +2,7 @@ module "alma" {
   source = "./vm"
 
   name = "alma"
-  image = var.alma_10G_node_1
+  image = var.alma_10G_node_2
   ip = 15
-  node = var.proxmox_node_1
+  node = var.proxmox_node_2
 }
--- terraform/galera.tf
+++ terraform/galera.tf
@@ -5,6 +5,7 @@ module "galera-1" {
   image = var.ubuntu_8G_node_1
   ip = 16
   node = var.proxmox_node_1
+  memory = 1024
 }
 
 module "galera-2" {
@@ -14,6 +15,7 @@ module "galera-2" {
   image = var.ubuntu_8G_node_2
   ip = 17
   node = var.proxmox_node_2
+  memory = 1024
 }
 
 module "galera-3" {
@@ -23,4 +25,5 @@ module "galera-3" {
   image = var.ubuntu_8G_node_2
   ip = 18
   node = var.proxmox_node_2
+  memory = 1024
 }
