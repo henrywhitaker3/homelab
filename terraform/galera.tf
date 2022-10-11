5b10d894fb20e69d9101a9b80cf3f684c2bf96b1
Henry Whitaker <henrywhitaker3@outlook.com>
2022-10-11 14:27:17 +0100

    update galera disk size

--- terraform/galera.tf
+++ terraform/galera.tf
@@ -2,7 +2,7 @@ module "galera-1" {
   source = "./vm"
 
   name = "galera-1"
-  image = var.ubuntu_8G
+  image = var.ubuntu_100G
   ip = 16
   node = 1
   memory = 1024
@@ -12,7 +12,7 @@ module "galera-2" {
   source = "./vm"
 
   name = "galera-2"
-  image = var.ubuntu_8G
+  image = var.ubuntu_100G
   ip = 17
   node = 2
   memory = 1024
@@ -22,7 +22,7 @@ module "galera-3" {
   source = "./vm"
 
   name = "galera-3"
-  image = var.ubuntu_8G
+  image = var.ubuntu_100G
   ip = 18
   node = 2
   memory = 1024
