5cc929822fae62f20ac6b35ec85263c6a1fe3672
Henry Whitaker <henrywhitaker3@outlook.com>
2022-10-11 16:13:34 +0100

    scrap galera - just a bigger mariadb

--- terraform/galera.tf
+++ terraform/galera.tf
@@ -1,29 +1,40 @@
-module "galera-1" {
+module "mariadb" {
   source = "./vm"
 
-  name = "galera-1"
+  name = "mariadb"
   image = var.ubuntu_100G
-  ip = 16
-  node = 1
-  memory = 1024
-}
-
-module "galera-2" {
-  source = "./vm"
-
-  name = "galera-2"
-  image = var.ubuntu_100G
-  ip = 17
+  ip = 10
   node = 2
-  memory = 1024
+  cores = 2
+  memory = 4096
 }
 
-module "galera-3" {
-  source = "./vm"
+# module "galera-1" {
+#   source = "./vm"
 
-  name = "galera-3"
-  image = var.ubuntu_100G
-  ip = 18
-  node = 2
-  memory = 1024
-}
+#   name = "galera-1"
+#   image = var.ubuntu_100G
+#   ip = 16
+#   node = 1
+#   memory = 1024
+# }
+
+# module "galera-2" {
+#   source = "./vm"
+
+#   name = "galera-2"
+#   image = var.ubuntu_100G
+#   ip = 17
+#   node = 2
+#   memory = 1024
+# }
+
+# module "galera-3" {
+#   source = "./vm"
+
+#   name = "galera-3"
+#   image = var.ubuntu_100G
+#   ip = 18
+#   node = 2
+#   memory = 1024
+# }
