a2bf51d09e0d34b3d88e92b0846220ba3a2e7abf
Henry Whitaker <henrywhitaker3@outlook.com>
2023-01-07 15:38:18 +0000

    add backup script to crog instead of cron

--- docker/templates/backup.sh
+++ docker/templates/backup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo $(date) - Start healthcheck
-curl -m 10 --retry 5 {{ healthchecks.backup }}/start
-echo ""
-
 echo $(date) - Mounting backups directory
 mount 10.0.0.9:/mnt/user/storage/docker /backups
 
@@ -12,7 +8,3 @@ rsync -avhH --delete --progress /home/{{ ansible_user }}/data /backups/
 
 echo $(date) - Unmounting backups directory
 umount /backups
-
-echo $(date) - Finish healthcheck
-curl -m 10 --retry 5 {{ healthchecks.backup }}
-echo ""
