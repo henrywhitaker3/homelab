3877ced92967e9644c9efc84e6885030d66bb535
Henry Whitaker <henrywhitaker3@outlook.com>
2023-02-09 02:44:09 +0000

    made the control plane ha

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -20,12 +20,23 @@ module "k8s-control-2" {
   node   = 2
 }
 
+module "k8s-control-3" {
+  source = "./vm"
+
+  name   = "k8s-control-3"
+  image  = local.ubuntu_22_04_100G
+  ip     = 22
+  cores  = 2
+  memory = 4096
+  node   = 2
+}
+
 module "k8s-worker-1" {
   source = "./vm"
 
   name   = "k8s-worker-1"
   image  = local.ubuntu_22_04_100G
-  ip     = 22
+  ip     = 23
   cores  = 2
   memory = 4096
   node   = 1
@@ -36,19 +47,8 @@ module "k8s-worker-2" {
 
   name   = "k8s-worker-2"
   image  = local.ubuntu_22_04_100G
-  ip     = 23
-  cores  = 2
-  memory = 4096
-  node   = 1
-}
-
-module "k8s-worker-3" {
-  source = "./vm"
-
-  name   = "k8s-worker-3"
-  image  = local.ubuntu_22_04_100G
   ip     = 24
   cores  = 2
   memory = 4096
-  node   = 2
+  node   = 1
 }
