be0e4c9ee6caeb6ce895b1bd1a0d16e0c828509a
Henry Whitaker <henrywhitaker3@outlook.com>
2023-04-22 16:19:34 +0100

    added lunar ubuntu image

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -3,6 +3,7 @@ locals {
   proxmox_node_2 = "proxmox-02"
   proxmox_host   = "10.0.0.150"
 
+  ubuntu_23_04_8G   = "ubuntu-23.04-cloud-8G"
   ubuntu_22_04_8G   = "ubuntu-22.04-cloud-8G"
   ubuntu_22_04_25G  = "ubuntu-22.04-cloud-25G"
   ubuntu_22_04_100G = "ubuntu-22.04-cloud-100G"
