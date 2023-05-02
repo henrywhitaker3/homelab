05c1050ca780018a6a5183c8b20aa0b43ff58514
Henry Whitaker <henrywhitaker3@outlook.com>
2023-05-02 20:45:07 +0100

    added more ubuntu 23.04 images

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -4,6 +4,8 @@ locals {
   proxmox_host   = "10.0.0.150"
 
   ubuntu_23_04_8G   = "ubuntu-23.04-cloud-8G"
+  ubuntu_23_04_25G   = "ubuntu-23.04-cloud-25G"
+  ubuntu_23_04_100G   = "ubuntu-23.04-cloud-100G"
   ubuntu_22_04_8G   = "ubuntu-22.04-cloud-8G"
   ubuntu_22_04_25G  = "ubuntu-22.04-cloud-25G"
   ubuntu_22_04_100G = "ubuntu-22.04-cloud-100G"
