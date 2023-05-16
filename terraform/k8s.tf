f529437c23cdbda9e614d076d678a301d26d2a88
Henry Whitaker <henrywhitaker3@outlook.com>
2023-05-16 15:02:55 +0100

    update k8s to bigger disks

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -2,7 +2,7 @@ module "k8s-control-1" {
   source = "./vm"
 
   name   = "k8s-control-1"
-  image  = local.ubuntu_22_04_100G
+  image  = local.ubuntu_22_04_200G
   ip     = 20
   cores  = 2
   memory = 8192
@@ -13,7 +13,7 @@ module "k8s-control-2" {
   source = "./vm"
 
   name   = "k8s-control-2"
-  image  = local.ubuntu_22_04_100G
+  image  = local.ubuntu_22_04_200G
   ip     = 21
   cores  = 2
   memory = 8192
@@ -24,7 +24,7 @@ module "k8s-worker-1" {
   source = "./vm"
 
   name   = "k8s-worker-1"
-  image  = local.ubuntu_22_04_100G
+  image  = local.ubuntu_22_04_200G
   ip     = 23
   cores  = 2
   memory = 8192
@@ -35,7 +35,7 @@ module "k8s-worker-2" {
   source = "./vm"
 
   name   = "k8s-worker-2"
-  image  = local.ubuntu_22_04_100G
+  image  = local.ubuntu_22_04_200G
   ip     = 24
   cores  = 2
   memory = 8192
@@ -46,7 +46,7 @@ module "k8s-worker-3" {
   source = "./vm"
 
   name   = "k8s-worker-3"
-  image  = local.ubuntu_22_04_100G
+  image  = local.ubuntu_22_04_200G
   ip     = 28
   cores  = 2
   memory = 8192
@@ -57,7 +57,7 @@ module "k8s-worker-4" {
   source = "./vm"
 
   name   = "k8s-worker-4"
-  image  = local.ubuntu_22_04_100G
+  image  = local.ubuntu_22_04_200G
   ip     = 22
   cores  = 2
   memory = 8192
--- terraform/locals.tf
+++ terraform/locals.tf
@@ -3,11 +3,9 @@ locals {
   proxmox_node_2 = "proxmox-02"
   proxmox_host   = "10.0.0.150"
 
-  ubuntu_23_04_8G   = "ubuntu-23.04-cloud-8G"
-  ubuntu_23_04_25G   = "ubuntu-23.04-cloud-25G"
-  ubuntu_23_04_100G   = "ubuntu-23.04-cloud-100G"
   ubuntu_22_04_8G   = "ubuntu-22.04-cloud-8G"
   ubuntu_22_04_25G  = "ubuntu-22.04-cloud-25G"
   ubuntu_22_04_100G = "ubuntu-22.04-cloud-100G"
+  ubuntu_22_04_200G = "ubuntu-22.04-cloud-200G"
   alma_9_10G        = "alma-9-cloud-10G"
 }
