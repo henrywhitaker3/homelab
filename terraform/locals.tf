7d80896d4005b33860c9ce38f851288432a6b529
Henry Whitaker <henrywhitaker3@outlook.com>
2023-09-26 15:07:40 +0100

    tflint

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,10 +1,7 @@
 locals {
-  proxmox_node_1 = "proxmox-01"
-  proxmox_node_2 = "proxmox-02"
   proxmox_host   = "10.0.0.150"
 
   ubuntu_22_04_8G   = "ubuntu-22.04-cloud-8G"
   ubuntu_22_04_25G  = "ubuntu-22.04-cloud-25G"
   ubuntu_22_04_200G = "ubuntu-22.04-cloud-200G"
-  ubuntu_22_04_300G = "ubuntu-22.04-cloud-300G"
 }
