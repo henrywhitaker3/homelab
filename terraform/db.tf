92505a9180f950b15e406d76e6aad82c4e9a0be1
Henry Whitaker <henrywhitaker3@outlook.com>
2023-11-13 14:51:06 +0000

    moved db over to new img

--- terraform/db.tf
+++ terraform/db.tf
@@ -2,22 +2,22 @@ module "mariadb-1" {
   source = "./vm"
 
   name   = "mariadb-1"
-  image  = local.ubuntu_22_04_25G
+  image  = local.ubuntu_22_04
   ip     = "10.0.0.15"
   cores  = 2
   memory = 2048
   node   = 1
-  disk_size = "27852M"
+  disk_size = "25G"
 }
 
 module "mariadb-2" {
   source = "./vm"
 
   name   = "mariadb-2"
-  image  = local.ubuntu_22_04_25G
+  image  = local.ubuntu_22_04
   ip     = "10.0.0.16"
   cores  = 2
   memory = 2048
   node   = 2
-  disk_size = "27852M"
+  disk_size = "25G"
 }
