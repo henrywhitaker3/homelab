ebbf611998687badbf2db96a1a51cedeff7577ee
Henry Whitaker <henrywhitaker3@outlook.com>
2023-11-13 15:37:50 +0000

    Update hosts role to handle cloud init hosts

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -2,34 +2,34 @@ module "pihole" {
   source = "./vm"
 
   name       = "pihole"
-  image      = local.ubuntu_22_04_8G
+  image      = local.ubuntu_22_04
   ip         = "10.0.0.2"
   memory     = 2048
   nameserver = "1.1.1.1 8.8.8.8"
   node       = 2
-  disk_size = "10444M"
+  disk_size = "8G"
 }
 
-module "haproxy-1" {
+module "lb-1" {
   source = "./vm"
 
   name   = "lb-1"
-  image  = local.ubuntu_22_04_8G
+  image  = local.ubuntu_22_04
   ip     = "10.0.0.4"
   memory = 512
   node   = 1
-  disk_size = "10444M"
+  disk_size = "8G"
 }
 
-module "haproxy-2" {
+module "lb-2" {
   source = "./vm"
 
   name   = "lb-2"
-  image  = local.ubuntu_22_04_8G
+  image  = local.ubuntu_22_04
   ip     = "10.0.0.5"
   memory = 512
   node   = 2
-  disk_size = "10444M"
+  disk_size = "8G"
 }
 
 module "vpn-1" {
