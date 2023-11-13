ba259c526131cdf1027ac5a3cb357f262af2ff58
Henry Whitaker <henrywhitaker3@outlook.com>
2023-11-13 15:01:41 +0000

    move over vpn boxes

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -36,20 +36,20 @@ module "vpn-1" {
   source = "./vm"
 
   name   = "vpn-1"
-  image  = local.ubuntu_22_04_8G
+  image  = local.ubuntu_22_04
   ip     = "10.0.0.11"
   memory = 512
   node   = 1
-  disk_size = "10444M"
+  disk_size = "8G"
 }
 
 module "vpn-2" {
   source = "./vm"
 
   name   = "vpn-2"
-  image  = local.ubuntu_22_04_8G
+  image  = local.ubuntu_22_04
   ip     = "10.0.0.12"
   memory = 512
   node   = 2
-  disk_size = "10444M"
+  disk_size = "8G"
 }
