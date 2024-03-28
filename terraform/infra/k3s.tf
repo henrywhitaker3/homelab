9c565ee357f3576d4fef141dcb933fded29871ad
Henry Whitaker <henrywhitaker3@outlook.com>
2024-03-28 18:37:22 +0000

    fix: update nameservers for k3s nodes

--- terraform/infra/k3s.tf
+++ terraform/infra/k3s.tf
@@ -4,6 +4,7 @@ module "k3s-control-1" {
   name      = "k3s-control-1"
   image     = local.ubuntu_22_04
   ip        = "10.0.0.23"
+  nameserver = "1.1.1.1 8.8.8.8"
   cores     = 3
   memory    = 12288
   node      = 1
@@ -18,6 +19,7 @@ module "k3s-control-2" {
   name      = "k3s-control-2"
   image     = local.ubuntu_22_04
   ip        = "10.0.0.22"
+  nameserver = "1.1.1.1 8.8.8.8"
   cores     = 3
   memory    = 12288
   node      = 2
@@ -32,6 +34,7 @@ module "k3s-worker-1" {
   name      = "k3s-worker-1"
   image     = local.ubuntu_22_04
   ip        = "10.0.0.20"
+  nameserver = "1.1.1.1 8.8.8.8"
   cores     = 2
   memory    = 8192
   node      = 1
@@ -46,6 +49,7 @@ module "k3s-worker-2" {
   name      = "k3s-worker-2"
   image     = local.ubuntu_22_04
   ip        = "10.0.0.21"
+  nameserver = "1.1.1.1 8.8.8.8"
   cores     = 2
   memory    = 8192
   node      = 2
