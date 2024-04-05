4992735cbf58256e36a8dd896e9d409d2a4d94e5
Henry Whitaker <henrywhitaker3@outlook.com>
2024-04-05 21:59:07 +0100

    fix: delete postgres bucket

--- terraform/post/outputs.tf
+++ terraform/post/outputs.tf
@@ -16,12 +16,3 @@ output "loki_secret_access_key" {
   value     = module.loki.secret_access_key
   sensitive = true
 }
-
-output "postgres_access_key_id" {
-  value     = module.loki.access_key_id
-  sensitive = true
-}
-output "postgres_secret_access_key" {
-  value     = module.postgres.secret_access_key
-  sensitive = true
-}
