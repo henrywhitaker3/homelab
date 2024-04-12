bf2655d391306a7d2548ea036292e4383fab4c4f
Henry Whitaker <henrywhitaker3@outlook.com>
2024-04-12 12:03:22 +0100

    chore: added script to quickly scale up/down media svcs

---
+++ scripts/media_scale.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+help() {
+    echo "Usage: [up|down]"
+}
+
+if [ $# -ne 1 ]; then
+    help
+    exit 1
+fi
+
+media=(
+sonarr
+radarr
+plex
+transmission
+)
+
+scale() {
+    sed -E -i "s/replicaCount: [0-9]+/replicaCount: $2/g" "argo/cluster/media/$1/chart/values.yaml"
+}
+
+for service in ${media[@]}; do
+    if [ $1 == "up" ]; then
+        scale $service 1
+    elif [ $1 == "down" ]; then
+        scale $service 0
+    else
+        help
+        exit 1
+    fi
+done
