b14c0acfd52e2109b377238942a8c9ba24ef6e4b
Henry Whitaker <henrywhitaker3@outlook.com>
2024-04-23 12:45:13 +0100

    fix: start adding things to talos cluster

--- scripts/bootstrap_secrets.sh
+++ scripts/bootstrap_secrets.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-for secret in argo/bootstrap/sops/*.sops.yaml; do
+if [[ $# -ne 1 ]]; then
+    echo "Provide a cluster context name first"
+fi
+
+for secret in "kubernetes/$1/bootstrap/sops/*.sops.yaml"; do
     sops --decrypt $secret | kubectl apply -f -
 done
