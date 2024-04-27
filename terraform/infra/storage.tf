2e13d28b1d77ee3bee06b9c8dbf55370d4c428c7
Henry Whitaker <henrywhitaker3@outlook.com>
2024-04-27 19:27:09 +0100

    chore: bumped up minio ram

--- terraform/infra/storage.tf
+++ terraform/infra/storage.tf
@@ -6,7 +6,7 @@ module "minio" {
   image     = local.ubuntu_22_04
   ips       = ["10.0.0.15", "10.0.0.16"]
   cores     = 1
-  memory    = 2048
+  memory    = 6144
   disk_size = "100G"
   nodes     = [1, 2]
 
