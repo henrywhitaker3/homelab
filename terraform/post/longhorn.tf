27b2675436c2f799965901131b72b82bc86c3b3e
Henry Whitaker <henrywhitaker3@outlook.com>
2024-05-06 16:19:27 +0100

    chore: added longhorn bucket

---
+++ terraform/post/longhorn.tf
@@ -0,0 +1,14 @@
+module "longhorn" {
+  source = "./bucket"
+
+  name    = "longhorn"
+  buckets = ["longhorn"]
+
+  acl       = "private"
+  permissions = [
+    "s3:DeleteObject",
+    "s3:GetObject",
+    "s3:ListBucket",
+    "s3:PutObject"
+  ]
+}
