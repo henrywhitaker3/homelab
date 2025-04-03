5477dcc5d43173720259ca676b62a162e27772f1
Henry Whitaker <henrywhitaker3@outlook.com>
2025-04-03 18:23:43 +0100

    refactor: move buckets to vars

--- terraform/post/buckets.tf
+++ terraform/post/buckets.tf
@@ -1,125 +1,69 @@
+variable "buckets" {
+  type = map(object({
+    name      = string
+    retention = optional(number, null)
+    acl       = optional(string, "private")
+    type      = string
+  }))
+  validation {
+    condition = length([
+      for key, value in var.buckets : true
+      if contains(["private", "public"], value.acl)
+    ]) == length(var.buckets)
+    error_message = "acl must be one of: private, public"
+  }
+  validation {
+    condition = length([
+      for key, value in var.buckets : true
+      if contains(["minio", "r2"], value.type)
+    ]) == length(var.buckets)
+    error_message = "type must be one of: minio, r2"
+  }
+}
+
+variable "minio_tokens" {
+  type = map(object({
+    name    = string
+    buckets = list(string)
+    write   = optional(bool, false)
+  }))
+  validation {
+    condition = length([
+      for key, value in var.minio_tokens : true
+      if length([
+        for b_key, b_value in value.buckets : true
+        if contains(keys(var.buckets), b_value)
+      ]) == length(value.buckets)
+    ]) == length(var.minio_tokens)
+    error_message = "buckets must be defined in var.buckets"
+  }
+}
+
+variable "r2_tokens" {
+  type = map(object({
+    buckets = list(string)
+    write   = optional(bool, false)
+  }))
+  validation {
+    condition = length([
+      for key, value in var.r2_tokens : true
+      if length([
+        for b_key, b_value in value.buckets : true
+        if contains(keys(var.buckets), b_value)
+      ]) == length(value.buckets)
+    ]) == length(var.r2_tokens)
+    error_message = "buckets must be defined in var.buckets"
+  }
+}
+
 locals {
-  buckets = {
-    dragonfly = {
-      name      = "dragonfly"
-      retention = 3
-      acl       = "private"
-      type      = "minio"
-    }
-    loki = {
-      name      = "loki-chunks"
-      acl       = "private"
-      type      = "minio"
-      retention = 60
-    }
-    mariadb = {
-      name = "mariadb"
-      acl  = "private"
-      type = "minio"
-    }
-    orderly = {
-      name      = "orderly"
-      acl       = "private"
-      type      = "minio"
-      retention = 3
-    }
-    pyroscope = {
-      name = "pyroscope"
-      acl  = "private"
-      type = "minio"
-    }
-    tempo = {
-      name = "tempo"
-      acl  = "private"
-      type = "minio"
-    }
-    yourbuild = {
-      name = "yourbuild"
-      acl  = "private"
-      type = "minio"
-    }
-    longhorn = {
-      name = "longhorn"
-      type = "r2"
-    }
-    crunchy = {
-      name = "crunchy"
-      type = "r2"
-    }
-    mariadb_r2 = {
-      name = "mariadb"
-      type = "r2"
-    }
-    books = {
-      name = "books"
-      type = "r2"
-    }
-  }
-
-  minio_tokens = {
-    dragonfly = {
-      name    = "dragonfly"
-      buckets = ["dragonfly"]
-      write   = true
-    }
-    loki = {
-      name    = "loki"
-      buckets = ["loki"]
-      write   = true
-    }
-    mariadb = {
-      name    = "mariadb"
-      buckets = ["mariadb"]
-      write   = true
-    }
-    orderly = {
-      name    = "orderly"
-      buckets = ["orderly"]
-      write   = true
-    }
-    pyroscope = {
-      name    = "pyroscope"
-      buckets = ["pyroscope"]
-      write   = true
-    }
-    tempo = {
-      name    = "tempo"
-      buckets = ["tempo"]
-      write   = true
-    }
-    yourbuild = {
-      name    = "yourbuild"
-      buckets = ["yourbuild"]
-      write   = true
-    }
-  }
-
-  r2_tokens = {
-    longhorn = {
-      buckets = ["longhorn"]
-      write   = true
-    }
-    crunchy = {
-      buckets = ["crunchy"]
-      write   = true
-    }
-    mariadb = {
-      buckets = ["mariadb"]
-      write   = true
-    }
-    books = {
-      buckets = ["books"]
-      write   = true
-    }
-  }
-
   minio_token_resources = {
-    for key, value in local.minio_tokens : key => {
+    for key, value in var.minio_tokens : key => {
       roots = [
-        for bucket in value.buckets : "arn:aws:s3:::${local.buckets[bucket].name}"
+        for bucket in value.buckets : "arn:aws:s3:::${var.buckets[bucket].name}"
       ]
       objs = [
-        for bucket in value.buckets : "arn:aws:s3:::${local.buckets[bucket].name}/*"
+        for bucket in value.buckets : "arn:aws:s3:::${var.buckets[bucket].name}/*"
       ]
     }
   }
@@ -136,7 +80,7 @@ locals {
   ]
 
   minio_token_policies = {
-    for key, value in local.minio_tokens : key => {
+    for key, value in var.minio_tokens : key => {
       policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -155,7 +99,7 @@ EOF
 
 resource "minio_s3_bucket" "bucket" {
   for_each = {
-    for key, value in local.buckets : key => value if value.type == "minio"
+    for key, value in var.buckets : key => value if value.type == "minio"
   }
 
   bucket = each.value.name
@@ -163,13 +107,13 @@ resource "minio_s3_bucket" "bucket" {
 }
 
 resource "minio_iam_user" "user" {
-  for_each = local.minio_tokens
+  for_each = var.minio_tokens
 
   name = each.value.name
 }
 
 resource "minio_iam_policy" "policy" {
-  for_each = local.minio_tokens
+  for_each = var.minio_tokens
 
   name       = each.value.name
   policy     = local.minio_token_policies[each.key].policy
@@ -177,14 +121,14 @@ resource "minio_iam_policy" "policy" {
 }
 
 resource "minio_iam_user_policy_attachment" "policy_attachment" {
-  for_each = local.minio_tokens
+  for_each = var.minio_tokens
 
   user_name   = minio_iam_user.user[each.key].id
   policy_name = minio_iam_policy.policy[each.key].id
 }
 
 resource "minio_iam_service_account" "svc_account" {
-  for_each = local.minio_tokens
+  for_each = var.minio_tokens
 
   target_user = minio_iam_user.user[each.key].name
   policy      = local.minio_token_policies[each.key].policy
@@ -194,7 +138,7 @@ resource "minio_iam_service_account" "svc_account" {
 
 resource "minio_ilm_policy" "lifecycle" {
   for_each = {
-    for key, value in local.buckets : key => value if value.type == "minio" && lookup(value, "retention", null) != null
+    for key, value in var.buckets : key => value if value.type == "minio" && lookup(value, "retention", null) != null
   }
 
   bucket = minio_s3_bucket.bucket[each.key].id
@@ -206,7 +150,7 @@ resource "minio_ilm_policy" "lifecycle" {
 
 resource "cloudflare_r2_bucket" "this" {
   for_each = {
-    for key, value in local.buckets : key => value if value.type == "r2"
+    for key, value in var.buckets : key => value if value.type == "r2"
   }
   account_id = lookup(each.value, "account_id", var.cloudflare_account_id)
   name       = each.value.name
@@ -215,7 +159,7 @@ resource "cloudflare_r2_bucket" "this" {
 data "cloudflare_api_token_permission_groups" "this" {}
 
 resource "cloudflare_api_token" "this" {
-  for_each = local.r2_tokens
+  for_each = var.r2_tokens
 
   name = format(
     "r2-%s-%s-%s",
@@ -233,13 +177,13 @@ resource "cloudflare_api_token" "this" {
       for bucket in each.value.buckets : format(
         "com.cloudflare.edge.r2.bucket.%s_default_%s",
         lookup(each.value, "account_id", var.cloudflare_account_id),
-        local.buckets[bucket].name,
+        var.buckets[bucket].name,
       ) => "*"
     }
   }
 }
 
 resource "random_bytes" "r2_tokens" {
-  for_each = local.r2_tokens
+  for_each = var.r2_tokens
   length   = 4
 }
--- terraform/post/outputs.tf
+++ terraform/post/outputs.tf
@@ -1,10 +1,10 @@
 output "access_key_ids" {
   value = merge(
     {
-      for key, value in local.minio_tokens : format("minio:%s", key) => minio_iam_service_account.svc_account[key].access_key
+      for key, value in var.minio_tokens : format("minio:%s", key) => minio_iam_service_account.svc_account[key].access_key
     },
     {
-      for key, value in local.r2_tokens : format("r2:%s", key) => cloudflare_api_token.this[key].id
+      for key, value in var.r2_tokens : format("r2:%s", key) => cloudflare_api_token.this[key].id
     }
   )
   sensitive = true
@@ -13,10 +13,10 @@ output "access_key_ids" {
 output "secret_access_keys" {
   value = merge(
     {
-      for key, value in local.minio_tokens : format("minio:%s", key) => minio_iam_service_account.svc_account[key].secret_key
+      for key, value in var.minio_tokens : format("minio:%s", key) => minio_iam_service_account.svc_account[key].secret_key
     },
     {
-      for key, value in local.r2_tokens : format("r2:%s", key) => sha256(cloudflare_api_token.this[key].value)
+      for key, value in var.r2_tokens : format("r2:%s", key) => sha256(cloudflare_api_token.this[key].value)
     }
   )
   sensitive = true
