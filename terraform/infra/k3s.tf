fdb8ab7cea67f84fe4c235215074ddedd8b8a6f4
Henry Whitaker <henrywhitaker3@outlook.com>
2025-07-30 16:47:02 +0100

    fix: update k3s vms to 6 cpu
    Signed-off-by: Henry Whitaker <henrywhitaker3@outlook.com>

--- terraform/infra/k3s.tf
+++ terraform/infra/k3s.tf
@@ -6,7 +6,7 @@ module "k3s-control" {
   image      = local.ubuntu_22_04
   ips        = ["10.0.0.23", "10.0.0.22", "10.0.0.19"]
   nameserver = "1.1.1.1 8.8.8.8"
-  cores      = 5
+  cores      = 6
   memory     = 16384
   disk_size  = "500G"
   nodes      = [1, 2, 3]
