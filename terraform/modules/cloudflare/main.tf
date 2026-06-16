1d1c5cd15fb7f46cf18113501473dad6f69c19bd
Henry Whitaker <henrywhitaker3@outlook.com>
2026-06-16 21:19:45 +0100

    fix: run tf-migrate
    Signed-off-by: Henry Whitaker <henrywhitaker3@outlook.com>

--- terraform/modules/cloudflare/main.tf
+++ terraform/modules/cloudflare/main.tf
@@ -1,30 +1,41 @@
 data "cloudflare_zone" "zone" {
-  name = var.zone
+  filter = {
+    name = var.zone
+  }
 }
 
-resource "cloudflare_record" "records" {
-  for_each = var.records
-
-  zone_id = data.cloudflare_zone.zone.zone_id
-  name    = each.value.name
-  content   = each.value.value
-  type    = each.value.type
-  proxied = each.value.proxied
-}
 
 locals {
   geo_block_expression = join(" ", [for name in var.geo_block : format("%q", name)])
 }
 
 resource "cloudflare_ruleset" "geo_block" {
-  zone_id     = data.cloudflare_zone.zone.zone_id
+  zone_id     = data.cloudflare_zone.zone.name_id
   name        = "Geo block"
   description = "Blocks access from specified coutries"
   kind        = "zone"
   phase       = "http_request_firewall_custom"
 
-  rules {
-    action     = "block"
-    expression = "(ip.geoip.country in {${local.geo_block_expression}})"
-  }
+  rules = [
+    {
+      action     = "block"
+      expression = "(ip.geoip.country in {${local.geo_block_expression}})"
+    }
+  ]
+}
+
+resource "cloudflare_dns_record" "records" {
+  for_each = var.records
+
+  zone_id = data.cloudflare_zone.zone.name_id
+  name    = each.value.name
+  content = each.value.value
+  type    = each.value.type
+  proxied = each.value.proxied
+  ttl     = 1
+}
+
+moved {
+  from = cloudflare_record.records
+  to   = cloudflare_dns_record.records
 }
